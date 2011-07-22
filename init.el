32f57e05d8f14e11bf44bf06f3c35ec7df62adb7
Sébastien Gross <sebchezwamorg>
2011-07-22 14:50:22 +0200

    Add el-get-notify.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2011-07-22 12:36:03
+;; Last changed: 2011-07-22 14:50:05
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -216,8 +216,12 @@
   (eval-buffer)
   (kill-buffer))
 
-(message "Emacs loaded in %fs"
-	 (destructuring-bind (hi lo ms) (current-time)
-	   (+
-	    (- (+ hi lo) (+ (first before-init-time) (second before-init-time)))
-	    (/ (- ms (third before-init-time)) (expt 10.0 6)))))
+(let* ((time
+	(destructuring-bind (hi lo ms) (current-time)
+	  (+
+	   (- (+ hi lo) (+ (first before-init-time) (second before-init-time)))
+	   (/ (- ms (third before-init-time)) (expt 10.0 6)))))
+       (msg (format "Emacs loaded in %fs" time)))
+  (message msg)
+  (unless noninteractive
+    (el-get-notify "Emacs is ready." msg)))
