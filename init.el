c00c7cd1fac73d288fe1b3ba9d819529760aeddb
Sébastien Gross <sebchezwamorg>
2011-07-24 22:32:05 +0200

    Add Muse.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2011-07-23 22:43:58
+;; Last changed: 2011-07-23 23:10:58
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -165,6 +165,7 @@
 	      list-processes+
 	      magit
 	      mailq
+	      muse
 	      nagios-mode
 	      nognus
 	      offlineimap
