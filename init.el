43299dfadfb1ceeb338b9d06c252bc314438dd0e
Sébastien Gross <sebchezwamorg>
2011-07-25 16:51:02 +0200

    Add path to el-get emacs wiki recipes.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2011-07-25 09:58:58
+;; Last changed: 2011-07-25 16:50:39
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -36,6 +36,10 @@
 	desktop-base-file-name (concat ,tmp-dir "desktop")
 	desktop-base-lock-name (concat ,tmp-dir "desktop.lock")
 	desktop-path (list ,tmp-dir))))
+
+  (eval-after-load 'el-get
+    `(progn
+       (setq el-get-recipe-path-emacswiki (concat ,tmp-dir "emacswiki"))))
 
   (eval-after-load 'emms
     `(progn 
