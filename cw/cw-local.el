ed4be07000de66368b3b4ff94c71acfe7e072e1b
Sébastien Gross <sebchezwamorg>
2011-07-25 09:55:07 +0200

    Do not reconnect on erc connection lost.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2011-07-24 22:31:28
+;; Last changed: 2011-07-25 09:53:28
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -125,7 +125,7 @@
 (eval-after-load 'erc
   '(progn
      (setq 
-      erc-server-auto-reconnect t
+      erc-server-auto-reconnect nil
       erc-kill-server-buffer-on-quit t)
      (set-face-attribute 'erc-notice-face nil :foreground "#ad7fa8")
      (set-face-attribute 'erc-input-face nil :foreground "#babdb6")
