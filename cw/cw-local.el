cc1b6c6b3957fd75e6429100099ea2872cc1fdee
Sébastien Gross <sebchezwamorg>
2011-08-06 01:31:03 +0200

    Add input method toggle.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2011-08-06 01:08:59
+;; Last changed: 2011-08-06 01:30:43
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -537,7 +537,16 @@ would be used if applicable ad remove CLEAR tag.
       ("!" [" !"])       ("!!" ["!"])
       (";" [" ;"])       (";;" [";"])
       (":" [" :"])       ("::" [":"])
-      )))
+      )
+
+     (defun cw:toggle-input-method ()
+       "Toggle between `french-cw-postfix' method  and nil"
+       (interactive)
+       (if (string= current-input-method "french-cw-postfix")
+	   (inactivate-input-method)
+	 (set-input-method "french-cw-postfix")))
+
+     (global-set-key (kbd "<f9>") 'cw:toggle-input-method)))
 
  ;; r
 (eval-after-load 'rainbow-delimiters
