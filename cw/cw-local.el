0470d4280f5f897fd446e2540c960c0a28279bb1
Sébastien Gross <sebchezwamorg>
2011-09-07 16:26:37 +0200

    Configure ibuffer.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -352,6 +352,44 @@
 	     try-complete-lisp-symbol))))
 
  ;; i
+(eval-after-load 'ibuffer
+  '(progn
+     (setq
+      ibuffer-saved-filter-groups
+      '(("default"
+	 ("dired" (mode . dired-mode))
+	 ("erc" (mode . erc-mode))
+	 ("emacs" (or
+		   (name . "^\\*scratch\\*$")
+		   (name . "^\\*Messages\\*$")
+		   (mode . completion-list-mode)
+		   (name . "\\*ielm\\*")))
+	 ("lisp" (mode . emacs-lisp-mode))
+	 ("gnus" (or
+		  (mode . message-mode)
+		  (mode . bbdb-mode)
+		  (mode . mail-mode)
+		  (mode . gnus-group-mode)
+		  (mode . gnus-summary-mode)
+		  (mode . gnus-article-mode)
+		  (name . "^\\.bbdb$")
+		  (name . "^\\.newsrc-dribble")
+		  (name . "^\\*nnimap ")
+		  (name . "^\\*imap log\\*")))
+	 ("org" (mode . org-mode))
+	 ("emms" (or
+		  (mode . emms)))
+	 ("hidden" (or
+		    (name . "^ "))))))
+     (add-hook 'ibuffer-mode-hook
+	       (lambda ()
+		 (ibuffer-switch-to-saved-filter-groups "default")))
+     (defadvice ibuffer (around cw:ibuffer activate)
+       "Open ibuffer with cursour pointed to most recent buffer name."
+       (let ((recent-buffer-name (buffer-name)))
+	 ad-do-it
+	 (ibuffer-jump-to-buffer recent-buffer-name)))))
+
 (eval-after-load 'ido
   '(progn
      ;; ido configuration
