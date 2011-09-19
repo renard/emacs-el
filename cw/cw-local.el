5f294018e1c3f6185596d37b3076c79605b6d173
Sébastien Gross <sebchezwamorg>
2011-09-19 15:19:49 +0200

    Add anything-config in requirements after anything loads.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2011-09-19 15:06:16
+;; Last changed: 2011-09-19 15:19:07
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -40,6 +40,9 @@
 	    "#ad7fa8" "cyan3" "#eeeeec"]
 	   ansi-color-map (ansi-color-make-color-map))))
 
+(eval-after-load 'anything
+  '(progn
+       (require 'anything-config nil t)))
  ;; b
 (eval-after-load 'browse-url
   '(progn
