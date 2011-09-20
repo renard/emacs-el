04be7abf4aebb9af57667b63a34e3e8d2e15887d
Sébastien Gross <sebchezwamorg>
2011-09-20 21:34:08 +0200

    Fix buffer name check.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-functions.el
+++ cw/cw-functions.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2011-09-19 14:35:08
+;; Last changed: 2011-09-20 21:33:31
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -220,7 +220,7 @@ kill related buffers."
        (not (erc-buffer-list))
        (let (do-reconnect)
 	 (mapcar (lambda(x)
-		   (when (string= "*irc: " (substring (buffer-name x) 0 6))
+		   (when (string-match "^\\*irc: " (buffer-name x))
 		     (unless (get-buffer-process x)
 		       (setq do-reconnect t))))
 		 (erc-buffer-list))
