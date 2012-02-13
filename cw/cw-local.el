50ee07e5c288f79383a9aa66a06717ea174630b8
Sébastien Gross <sebchezwamorg>
2012-02-13 23:42:36 +0100

    Exclude server buffer from erc track.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -187,6 +187,7 @@
      (setq
       erc-track-exclude-types '("JOIN" "PART" "QUIT" "NICK" "MODE" "333" "353")
       erc-track-use-faces t
+      erc-track-exclude-server-buffer t
       erc-track-priority-faces-only 'all)))
 
 (eval-after-load 'escreen
