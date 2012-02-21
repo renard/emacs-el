e9fa2360a7b5c8fc325f4602ba949f686a0d29ae
Sébastien Gross <sebchezwamorg>
2012-02-21 11:52:53 +0100

    Add slime requirement.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2012-02-21 11:50:35
+;; Last changed: 2012-02-21 11:52:33
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -1116,6 +1116,8 @@ works and run `anything-other-buffer'."
   ;; notification-notify
   (require 'notifications nil t)
 
+  ;; slime support
+  (require 'slime nil t)
 
   (ido-mode t)
   (show-paren-mode t)
