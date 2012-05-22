6cd422685dbbfd5919d06dab0733aaefbbcba808
Sébastien Gross <sebchezwamorg>
2012-05-22 16:56:12 +0200

    Add custom file.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2012-04-05 16:26:05
+;; Last changed: 2012-04-18 01:50:56
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -53,7 +53,8 @@
   (eval-after-load 'cus-edit
     `(progn
        ;; Do not save custom variables.
-       (setq custom-file (concat ,tmp-dir "custom"))))
+       (setq custom-file (concat ,tmp-dir "custom"))
+       (load custom-file)))
 
   (eval-after-load 'desktop
     `(progn
