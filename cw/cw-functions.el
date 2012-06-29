bc1d07083a1cd694278c60cb191d833f81f714c5
Sébastien Gross <sebchezwamorg>
2012-06-29 23:37:52 +0200

    Add uniquify functions.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-functions.el
+++ cw/cw-functions.el
@@ -565,6 +565,22 @@ CMD is a string used to call the contact. By default it is set to:
 ;;;###autoload
 (defmacro time (form)
   `(%time (lambda () ,form)))
+(defun uniquify-all-lines-region (start end)
+    "Find duplicate lines in region START to END keeping first occurrence."
+    (interactive "*r")
+    (save-excursion
+      (let ((end (copy-marker end)))
+        (while
+            (progn
+              (goto-char start)
+              (re-search-forward "^\\(.*\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
+          (replace-match "\\1\n\\2")))))
+
+(defun uniquify-all-lines-buffer ()
+    "Delete duplicate lines in buffer and keep first occurrence."
+    (interactive "*")
+    (uniquify-all-lines-region (point-min) (point-max)))
+
 ;;;###autoload
 (defun cw:slime:ccl ()
   "Configure slime for CCL."
