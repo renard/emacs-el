e77581ac01bb41ef5b4ccdc6688e817aec755c75
Sébastien Gross <sebchezwamorg>
2012-06-29 18:47:27 +0200

    Update browse-url for macosx
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -50,13 +50,23 @@
  ;; b
 (eval-after-load 'browse-url
   '(progn
-     (setq
-      browse-url-generic-program
-      (if (running-macosxp)
-	  "/Applications/Safari.app/Contents/MacOS/Safari"
-	"raise-x-www-browser")
-      browse-url-generic-args nil
-      browse-url-browser-function 'browse-url-generic)))
+     (if (running-macosxp)
+       (progn
+	 (defun browse-url-default-macosx-browser (url &optional new-window)
+	   (interactive (browse-url-interactive-arg "URL: "))
+	   (if (and new-window (>= emacs-major-version 23))
+	       (ns-do-applescript
+		(format (concat "tell application \"Safari\" to make document with properties {URL:\"%s\"}\n"
+				"tell application \"Safari\" to activate") url))
+	     (start-process (concat "open " url) nil "open" url)))
+	 (setq browse-url-browser-function 'browse-url-default-macosx-browser))
+       (setq
+	browse-url-generic-program
+	(if (running-macosxp)
+	    "/Applications/Safari.app/Contents/MacOS/Safari"
+	  "raise-x-www-browser")
+	browse-url-generic-args nil
+	browse-url-browser-function 'browse-url-generic))))
 
 (eval-after-load 'buffer-move
   '(progn
