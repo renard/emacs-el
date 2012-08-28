061054dd80dd7b0ed8c6e1fdc809579e56e923ac
Sébastien Gross <sebchezwamorg>
2012-08-28 10:20:58 +0200

    Add bbdb.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2012-08-27 15:10:42
+;; Last changed: 2012-08-27 23:37:56
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -49,6 +49,10 @@
   (eval-after-load 'abbrev
     `(progn
        (setq abbrev-file-name (concat ,tmp-dir "abbrev_defs"))))
+
+  (eval-after-load 'bbdb
+    `(progn
+       (setq bbdb-file (concat ,tmp-dir "bbdb"))))
 
   (eval-after-load 'cus-edit
     `(progn
@@ -214,6 +218,7 @@
 
 	      adoc-mode
 	      anything
+	      bbdb
 	      browse-kill-ring
 	      buffer-move
 	      cisco-router-mode
