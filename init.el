ad5669ef7830e995a664c737de9adc156654aaa3
Sébastien Gross <sebchezwamorg>
2012-09-26 10:31:36 +0200

    Add expand-region and multiple-cursors.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 ;; Author: Sebastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, configuration
 ;; Created: 2010-12-09
-;; Last changed: 2012-09-02 23:58:34
+;; Last changed: 2012-09-26 10:31:12
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -168,6 +168,7 @@
 	      dpans2texi
 	      emacs-http-server
 	      escreen
+	      expand-region
 	      fill-column-indicator
 	      filladapt
 	      google-maps
@@ -180,6 +181,7 @@
 	      magit
 	      mailq
 	      maxframe
+	      multiple-cursors
 	      muse
 	      nagios-mode
 	      offlineimap
