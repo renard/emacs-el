47f630ad338b4e2fef6ac76846cd4eccf4fa40a5
Sébastien Gross <sebchezwamorg>
2012-09-27 10:58:00 +0200

    Indent code.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-local.el
+++ cw/cw-local.el
@@ -64,15 +64,15 @@
 (eval-after-load 'browse-url
   '(progn
      (if (running-macosxp)
-       (progn
-	 (defun browse-url-default-macosx-browser (url &optional new-window)
-	   (interactive (browse-url-interactive-arg "URL: "))
-	   (if (and new-window (>= emacs-major-version 23))
-	       (ns-do-applescript
-		(format (concat "tell application \"Safari\" to make document with properties {URL:\"%s\"}\n"
-				"tell application \"Safari\" to activate") url))
-	     (start-process (concat "open " url) nil "open" url)))
-	 (setq browse-url-browser-function 'browse-url-default-macosx-browser))
+	 (progn
+	   (defun browse-url-default-macosx-browser (url &optional new-window)
+	     (interactive (browse-url-interactive-arg "URL: "))
+	     (if (and new-window (>= emacs-major-version 23))
+		 (ns-do-applescript
+		  (format (concat "tell application \"Safari\" to make document with properties {URL:\"%s\"}\n"
+				  "tell application \"Safari\" to activate") url))
+	       (start-process (concat "open " url) nil "open" url)))
+	   (setq browse-url-browser-function 'browse-url-default-macosx-browser))
        (setq
 	browse-url-generic-program
 	(if (running-macosxp)
