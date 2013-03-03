c3e03ea2b338a6da97f462511a7b6ccf12ceacae
Sébastien Gross <sebchezwamorg>
2013-03-03 22:49:53 +0100

    Update desktop configuration.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/packages.d/desktop.load.el
+++ cw/packages.d/desktop.load.el
@@ -1,4 +1,5 @@
 (setq
  desktop-base-file-name (concat cw:tmp-dir "desktop")
  desktop-base-lock-name (concat cw:tmp-dir "desktop.lock")
- desktop-path (list cw:tmp-dir))
+ desktop-path (list cw:tmp-dir)
+ desktop-restore-eager 20)
