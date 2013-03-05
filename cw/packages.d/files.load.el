9309dbf1426a08423aa40f25d1226271ea95c79a
Sébastien Gross <sebchezwamorg>
2013-03-05 20:03:23 +0100

    Set executable bit on scripts files.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/packages.d/files.load.el
+++ cw/packages.d/files.load.el
@@ -1,3 +1,21 @@
+;;;###autoload
+(defun cw:files:make-buffer-file-executable-if-script-p ()
+  "Make file executable according to umask if not already executable.
+If file already has any execute bits set at all, do not change existing
+file modes."
+  (and (save-excursion
+	 (save-restriction
+	   (widen)
+	   (goto-char (point-min))
+	   (save-match-data
+	     (looking-at "^#!"))))
+       (let ((current-mode (file-modes (buffer-file-name)))
+	     (add-mode (logand ?\111 (default-file-modes))))
+	 (or (/= (logand ?\111 current-mode) 0)
+	     (zerop add-mode)
+	     (set-file-modes (buffer-file-name)
+			     (logior current-mode add-mode))))))
+
 (setq
  backup-directory-alist `((".*" . ,(concat cw:tmp-dir "backup"))))
 
@@ -6,3 +24,5 @@
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
 (add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
 
+(add-hook 'after-save-hook 'cw:files:make-buffer-file-executable-if-script-p)
+(add-hook 'before-save-hook 'time-stamp)
