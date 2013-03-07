cd05ee7f100a1de524f19ee149bcfe436897a739
Sébastien Gross <sebchezwamorg>
2013-03-07 02:21:45 +0100

    Explicitly call ido-completion-help original form.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/packages.d/ido.load.el
+++ cw/packages.d/ido.load.el
@@ -8,9 +8,17 @@
  ido-default-buffer-method 'selected-window)
 
 (defadvice ido-completion-help (around cw:ido-completion-help activate)
-  "Do not create \"Ido Completion Help\" buffer.")
+  "Do not create \"Ido Completion Help\" buffer except if
+  explicitly called by `cw:ido-completion-help-orig'."
+  (when (eq this-command 'cw:ido-completion-help-orig)
+    ad-do-it))
+
+(defun cw:ido-completion-help-orig ()
+  "Call original `ido-completion-help' function."
+  (interactive)
+  (ido-completion-help))
 
 (defun cw:ido-init-keys ()
   "Add some usefull ido bindings."
-  (define-key ido-common-completion-map (kbd "?") 'ad-Orig-ido-completion-help))
+  (define-key ido-common-completion-map (kbd "?") 'cw:ido-completion-help-orig))
 (add-hook 'ido-setup-hook 'cw:ido-init-keys)
