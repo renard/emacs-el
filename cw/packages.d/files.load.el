a4154c198b52fc7e188d371e1f31864bb43d4668
Sébastien Gross <sebchezwamorg>
2013-03-07 01:26:26 +0100

    Move skeletons to files module.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/packages.d/files.load.el
+++ cw/packages.d/files.load.el
@@ -16,6 +16,36 @@ file modes."
 	     (set-file-modes (buffer-file-name)
 			     (logior current-mode add-mode))))))
 
+(defvar cw:new-file-tpl-maker "new-file-tpl"
+  "String to be inserted as new file skeleton by
+  `cw:files:insert-template-maybe'.")
+
+;;;###autoload
+(defun cw:files:insert-template-maybe ()
+  "Insert a skeleton if `cw:new-file-tpl-maker' is found as a
+`yasnippet'."
+  (when
+      (and
+       (not noninteractive)
+       (buffer-file-name)
+       (not (file-exists-p (buffer-file-name)))
+       (= (point-max) 1))
+    (unless (functionp 'yas-expand)
+      (require 'yasnippet))
+    (let ((mode (symbol-name major-mode)))
+      (loop for d in yas-snippet-dirs
+	    when (file-exists-p
+		  (format "%s/%s/%s.yasnippet" d mode
+			  cw:new-file-tpl-maker))
+	    do (progn
+		 ;;(message "Found: %s/%s/%s.yasnippet" d mode cw:new-file-tpl-maker)
+		 (insert cw:new-file-tpl-maker)
+		 ;; reload templates before expansion
+		 ;; why is this needed?
+		 (yas-reload-all)
+		 (yas-expand))
+	    and return t))))
+
 (setq
  backup-directory-alist `((".*" . ,(concat cw:tmp-dir "backup"))))
 
@@ -26,7 +56,4 @@ file modes."
 
 (add-hook 'after-save-hook 'cw:files:make-buffer-file-executable-if-script-p)
 (add-hook 'before-save-hook 'time-stamp)
-(add-hook 'find-file-hook
-	  (lambda()
-	    (when (functionp 'cw:yasnippet:insert-maybe)
-	      (cw:yasnippet:insert-maybe))))
+(add-hook 'find-file-hook 'cw:files:insert-template-maybe)
--- cw/packages.d/yasnippet.load.el
+++ cw/packages.d/yasnippet.load.el
@@ -1,25 +1,4 @@
-;;;###autoload
-(defun cw:yasnippet:insert-maybe ()
-  ""
-  (interactive)
-  (when
-      (and
-       (not noninteractive)
-       (buffer-file-name)
-       (not (file-exists-p (buffer-file-name)))
-       (= (point-max) 1))
-    (let ((mode (symbol-name major-mode)))
-      (loop for d in yas-snippet-dirs
-	    when (file-exists-p
-		  (format "%s/%s/new-file-tpl.yasnippet" d mode))
-	    do (progn
-		 ;;(message "Found: %s/%s/new-file-tpl.yasnippet" d mode)
-		 (insert "new-file-tpl")
-		 ;; reload templates before expansion
-		 (yas-reload-all)
-		 (yas-expand))
-	  and return t))))
-
+;; do not use ~/.emacs.d/snippets as a directory
 (setq yas-snippet-dirs
       (list (concat user-emacs-directory "templates")
 	    (when yas--load-file-name
