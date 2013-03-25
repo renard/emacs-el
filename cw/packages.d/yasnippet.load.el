8f77ab99c3c5b88e194f9a062761c4101c7e0ef2
Sébastien Gross <sebchezwamorg>
2013-03-25 18:48:15 +0100

    Add yas-exit-all-snippets.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/packages.d/yasnippet.load.el
+++ cw/packages.d/yasnippet.load.el
@@ -4,5 +4,9 @@
 	    (when yas--load-file-name
 	      (concat (file-name-directory yas--load-file-name) "snippets"))))
 
+(defun cw:yas-exit-all-snippets()
+  "Execute `yas-exit-all-snippets' within a `save-excursion' block."
+  (interactive)
+  (save-excursion (yas-exit-all-snippets)))
 
-
+(define-key yas-keymap (kbd "C-g") 'cw:yas-exit-all-snippets)
