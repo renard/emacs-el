cf8ba05ebe3d989c3b1c4f1c24d3c77165b8d23e
Sébastien Gross <sebchezwamorg>
2013-05-30 19:41:11 +0200

    Add cl requirement.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/cw-functions.el
+++ cw/cw-functions.el
@@ -5,7 +5,7 @@
 ;; Author: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 ;; Keywords: emacs, 
 ;; Created: 2013-03-05
-;; Last changed: 2013-05-30 19:37:49
+;; Last changed: 2013-05-30 19:41:01
 ;; Licence: WTFPL, grab your copy here: http://sam.zoy.org/wtfpl/
 
 ;; This file is NOT part of GNU Emacs.
@@ -16,7 +16,8 @@
 
 ;;; Code:
 
-
+(eval-when-compile
+  (require 'cl))
 
 ;; http://www.emacswiki.org/emacs/ParenthesisMatching
 ;;;###autoload
