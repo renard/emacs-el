bb69bedd6be4a21d1c689d185b702349c55695ac
Sébastien Gross <sebchezwamorg>
2013-07-02 16:57:25 +0200

    Add cw:term.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/packages.d/term.load.el
+++ cw/packages.d/term.load.el
@@ -52,6 +52,60 @@
    '(:family "Terminus" :height 120)))
   (buffer-face-mode))
 
+;;;###autoload
+(defun cw:term (&optional sudo)
+  "Run `term' with specific settings."
+  "Run terminal in current buffer directory."
+  (interactive "P")
+  (cw:with-parse-directory
+   sudo
+   (progn
+     (unless (boundp 'explicit-shell-file-name) (require 'term nil t))
+     (let ((shell (or explicit-shell-file-name
+		      (getenv "ESHELL")
+		      (getenv "SHELL")
+		      "/bin/sh"))
+	   (sh-cmd)
+	   (cmd ""))
+       ;; Create shell argument to connect to remote host.
+       (when host (setq sh-cmd (concat sh-cmd (format "ssh %s -t" host))))
+       (when user (setq sh-cmd (concat sh-cmd (format " sudo -u %s -s" user))))
+       (when sh-cmd (setq sh-cmd `("-c" ,sh-cmd)))
+
+       ;; Prepare commands to change directory
+       (when localname (setq cmd (format "%s cd %s;" cmd localname)))
+       (when host
+	 (setq cmd (concat cmd " "
+			   ;; enable remote directory tracking
+			   "function prompt_cmd { "
+			   "echo -e \"\\033AnSiTu\" ${TRAMP_USERNAME-$(whoami)};"
+			   "echo -e \"\\033AnSiTc\" $(pwd);"
+			   "echo -e \"\\033AnSiTh\" ${TRAMP_HOSTNAME-" host "};"
+			   " };"
+			   "export PROMPT_COMMAND=prompt_cmd;")))
+       (setq cmd (concat cmd " "
+			 ;; don't store these lines into shell history
+			 "history -d $((HISTCMD - 1)); clear; \n"))
+       ;; Start shell
+       (switch-to-buffer
+	(apply  'make-term
+	 (append
+	  (list
+	   (if host (concat "ssh " host)
+	     (concat (file-name-nondirectory shell) " " default-directory))
+	   shell nil)
+	  sh-cmd)))
+
+       (set-process-sentinel
+	(get-buffer-process (current-buffer))
+	(lambda (proc change)
+	  (when (eq (process-status proc) 'exit)
+	    (kill-buffer (process-buffer proc)))))
+
+       (term-mode)
+       (term-char-mode)
+       (process-send-string (get-buffer-process (current-buffer)) cmd)))))
+
 (define-key term-raw-map (kbd "M-x") 'execute-extended-command)
 (define-key term-raw-map (kbd "M-'") 'ido-switch-buffer)
 (define-key term-raw-map (kbd "C-y") 'term-paste)
