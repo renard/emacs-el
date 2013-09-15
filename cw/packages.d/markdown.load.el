a45e9ec3575124c3931a4a44ae44bae5cdd225bf
Sébastien Gross <sebchezwamorg>
2013-09-16 00:21:00 +0200

    Convert markdown buffer to html.
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- cw/packages.d/markdown.load.el
+++ cw/packages.d/markdown.load.el
@@ -1,3 +1,18 @@
 (define-key markdown-mode-map (kbd "M-[") 'escreen-goto-prev-screen)
 (define-key markdown-mode-map (kbd "M-]") 'escreen-goto-next-screen)
 
+(defun cw:markdown-buffer-to-html()
+  "Convert current buffer to HTML"
+  (interactive)
+  (let ((buff-src (current-buffer))
+	(file (file-name-sans-extension (buffer-file-name)))
+	(buff-out (get-buffer-create "*PANDOC*")))
+    (with-current-buffer buff-out
+      (erase-buffer))
+    (call-process-region (point-min) (point-max)
+			 "pandoc" nil buff-out nil "-s")
+    (with-temp-file (concat file ".html")
+      (insert (with-current-buffer buff-out (buffer-string))))))
+
+
+
