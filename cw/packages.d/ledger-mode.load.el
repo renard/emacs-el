78f9e18f083a3829ae8f7f60703c39eb5535579c
Sébastien Gross <sebchezwamorg>
2014-03-25 03:00:10 +0100

    Add ledger mode.

---
+++ cw/packages.d/ledger-mode.load.el
@@ -0,0 +1,6 @@
+
+(set-face-attribute 'ledger-font-xact-highlight-face nil :background "#32383a")
+(set-face-attribute 'ledger-occur-xact-face nil :background "grey70")
+(setq
+ ledger-reconcile-default-commodity "EUR"
+ ledger-post-amount-alignment-column 76)
