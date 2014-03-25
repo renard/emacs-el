4b8b8a9aa67b0ca6de50eb0a90c6d2e9ebd0d6a7
Sébastien Gross <sebchezwamorg>
2014-03-25 03:00:49 +0100

    Ad env preload

---
+++ cw/packages.d/env.preload.el
@@ -0,0 +1,6 @@
+;; Problem with TRAMP mode
+;; Control Path too long error
+;; TMPDIR variable is really large
+;; http://lists.macosforge.org/pipermail/macports-tickets/2011-June/084295.html
+(setenv "TMPDIR" "/tmp")
+
