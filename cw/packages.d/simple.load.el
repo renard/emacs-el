06ec838ceb244aa2e0eb44a037c9bc106d311b2e
Sébastien Gross <sebchezwamorg>
2014-04-28 16:05:16 +0200

    Add feedback for goto-line

--- cw/packages.d/simple.load.el
+++ cw/packages.d/simple.load.el
@@ -14,3 +14,15 @@
       ;; This is an empty line, delete its spaces
       (when (looking-back "[ \t]+$")
 	(delete-horizontal-space)))))
+
+;; Remap goto-line to show temporary the line number.
+;; http://whattheemacsd.com//key-bindings.el-01.html
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
