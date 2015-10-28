4f81c4cf81cffdc393e73e137113befe3b65d45e
Sébastien Gross <sebchezwamorg>
2015-10-28 14:19:52 +0100

    Change message-mode-hook order

--- cw/packages.d/message.load.el
+++ cw/packages.d/message.load.el
@@ -1,7 +1,7 @@
 (setq message-signature-insert-empty-line t)
 
 (add-hook 'message-mode-hook 'cw:gnus:configure-group)
-(add-hook 'message-mode-hook 'flyspell-mode)
-(add-hook 'message-mode-hook 'flyspell-buffer)
+(add-hook 'message-mode-hook 'flyspell-mode t)
+(add-hook 'message-mode-hook 'flyspell-buffer t)
 
 (define-key message-mode-map (kbd "<tab>") 'osx-contacts-complete-address)
